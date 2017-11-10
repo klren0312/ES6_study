32d1f4d11ccb2b01a3356a70b4818c5c34e714d5
zzes <kuanglong0312@sina.com>
2017-11-10 17:14:59 +0800

    template literals

---
+++ strings/templateLiterals.js
@@ -0,0 +1,6 @@
+var text = "This string contains \"double\" which are escaped"
+
+var text2 = `This string contains "double" which are escaped`
+
+console.log(text+"\n")
+console.log(text2+"\n")
---
+++ strings/templateLiterals2.js
@@ -0,0 +1,12 @@
+//ES5
+// var name = 'Tiger'
+// var age = 13
+
+// console.log('my cat is named ' + name + ' and is ' + age + ' years old')
+
+//ES6
+const name = 'Tiger'
+const age = 133
+
+console.log(`my cat is named ${name} and is ${age} years old`)
+console.log(`the time and date is ${new Date().toLocaleString()}`)
