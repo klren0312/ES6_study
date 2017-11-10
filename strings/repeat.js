76c5576a7e97e13ce97dfb195987e877c39118de
zzes <kuanglong0312@sina.com>
2017-11-10 17:14:44 +0800

    repeat

---
+++ strings/repeat.js
@@ -0,0 +1,20 @@
+/**
+ * ES5
+ */
+// function repeat(string, count){
+//   var strings = []
+//   while(strings.length < count){
+//     strings.push(string)
+//   }
+//   return strings.join('')
+// }
+
+// var string = 'kl'
+// var afterstring = repeat(string,4)
+// console.log(afterstring)
+
+/**
+ * ES6
+ */
+var string = 'kl'
+console.log(string.repeat(4))
