d99ae73996120ee50affc626dd0c708ea684bca3
zzes <kuanglong0312@sina.com>
2017-11-11 22:32:42 +0800

    parameter

---
+++ parameters/defaultParam.js
@@ -0,0 +1,4 @@
+function addTwoNumber(x=0,y=0){
+  return x+y
+}
+console.log(addTwoNumber())
---
+++ parameters/restParam.js
@@ -0,0 +1,10 @@
+function logArguments(...args){
+  for(let arg of args){
+    console.log(arg)
+  }
+}
+let a = 1
+let b = "ag"
+let c = "eafewfa"
+let d = 3234244
+logArguments(a,b,c,d)
