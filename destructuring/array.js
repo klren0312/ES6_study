4b29e9b7475820fbcf8d60dbf39383e82b0f633b
zzes <kuanglong0312@sina.com>
2017-11-11 22:32:55 +0800

    destructuring

---
+++ destructuring/array.js
@@ -0,0 +1,13 @@
+//ES5
+// var arr = [1,2,3,4]
+// var a = arr[0]
+// var b = arr[1]
+// var c = arr[2]
+// var d = arr[3]
+
+
+//ES6
+let [a,b,c,d] = [1,2,3,4]
+
+console.log(a)
+console.log(b)
---
+++ destructuring/object.js
@@ -0,0 +1,19 @@
+//ES5
+// var luck = {
+//   occupation:'jedi',
+//   father:'anakin'
+// }
+
+// var occupation = luck.occupation
+// var father = luck.father
+
+//ES6
+let luck = {
+  occupation:'jedi',
+  father:'anakin'
+}
+
+let {occupation,father} = luck
+
+console.log(occupation)
+console.log(father)
