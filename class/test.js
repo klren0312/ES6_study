d5a4c13235016018b0d4ad7cbf596490121baf07
zzes <kuanglong0312@sina.com>
2017-11-11 22:33:24 +0800

    class

---
+++ class/test.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor(name,age,gender){
+    this.name = name
+    this.age = age
+    this.gender = gender
+  }
+  incrementAge(){
+    return this.age += 1
+  }
+}
+
+let person = new Person('atea',232,"rere")
+console.log(person.incrementAge())
